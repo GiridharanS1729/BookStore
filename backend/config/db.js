bb9da323ffce5467a427f7a75665ac691c070b14
GiridharanS1729 <giridharans1729@gmail.com>
2025-02-08 15:14:50 +0530

    backend issue fixed 08022025151444

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,19 @@
 const mongoose = require('mongoose');
-const url = process.env.MONGO_URI || "mongodb://localhost:27017/bookstore";
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 const connectDB = async () => {
   try {
-    await mongoose.connect(url);
-    console.log('Connected to MongoDB');
-  } catch (err) {
-    console.error(err.message);
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB Connected');
+  } catch (error) {
+    console.error('Database Connection Error:', error);
     process.exit(1);
   }
 };
+
 module.exports = connectDB;
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,8 @@
-require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
 const cors = require("cors");
+require('dotenv').config();
 app.use(cors());
 
 connectDB();
@@ -10,9 +10,11 @@ const PORT = process.env.PORT || 5000;
 const bookRoutes = require('./routes/bookRoutes');
 app.use(express.json());
 app.use('/books', bookRoutes);
+
 app.get('/', (req, res) => {
     res.send('Success running server');
 });
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
